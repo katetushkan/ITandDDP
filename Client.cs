5c806254f469763b0b52473067fa491f760fe472
sashaaladko <sasha.aladko@mail.ru>
2022-03-27 23:27:40 +0300

    lab 1

---
+++ Client.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    class Client
+    {
+        static int remotePort; 
+        static IPAddress ipAddress; 
+        static Socket listeningSocket; 
+
+        static void Main(string[] args)
+        {
+            Console.Write("enter client ip: ");
+            ipAddress = IPAddress.Parse(Console.ReadLine());
+            Console.Write("enter the port for sending messages: ");
+            remotePort = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("to sent message enter text and press Enter");
+            Console.WriteLine();
+
+            try
+            {
+                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); 
+                Task listeningTask = new Task(Listen); 
+                listeningTask.Start(); 
+
+                while (true) 
+                {
+                    string message = Console.ReadLine();
+
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    EndPoint remotePoint = new IPEndPoint(ipAddress, remotePort);
+                    listeningSocket.SendTo(data, remotePoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        private static void Listen()
+        {
+            try
+            {
+                IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 0); 
+                listeningSocket.Bind(localIP);
+
+                while (true)
+                {
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
+                    do
+                    {
+                        bytes = listeningSocket.ReceiveFrom(data, ref remoteIp);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (listeningSocket.Available > 0);
+
+                    IPEndPoint remoteFullIp = remoteIp as IPEndPoint;
+
+                    Console.WriteLine("{0}:{1} - {2}", remoteFullIp.Address.ToString(), remoteFullIp.Port, builder.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        private static void Close()
+        {
+            if (listeningSocket != null)
+            {
+                listeningSocket.Shutdown(SocketShutdown.Both);
+                listeningSocket.Close();
+                listeningSocket = null;
+            }
+
+            Console.WriteLine("server stopped");
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    class Server
+    {
+        static int localPort;
+        static Socket listeningSocket;
+
+        static List<IPEndPoint> clients = new List<IPEndPoint>();
+
+        static void Main(string[] args)
+        {
+            Console.Write("enter the port for receiving messages: ");
+            localPort = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            try
+            {
+                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Task listeningTask = new Task(Listen);
+                listeningTask.Start();
+                listeningTask.Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        private static void Listen()
+        {
+            try
+            {
+                IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), localPort);
+                listeningSocket.Bind(localIP);
+
+                while (true)
+                {
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+                    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
+                    do
+                    {
+                        bytes = listeningSocket.ReceiveFrom(data, ref remoteIp);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (listeningSocket.Available > 0);
+                    IPEndPoint remoteFullIp = remoteIp as IPEndPoint;
+
+                    Console.WriteLine("{0}:{1} - {2}", remoteFullIp.Address.ToString(), remoteFullIp.Port, builder.ToString());
+
+                    bool addClient = true;
+
+                    for (int i = 0; i < clients.Count; i++)
+                        if (clients[i].Address.ToString() == remoteFullIp.Address.ToString())
+                            addClient = false;
+
+                    if (addClient == true)
+                        clients.Add(remoteFullIp);
+
+                    BroadcastMessage(builder.ToString(), remoteFullIp.Address.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+
+        private static void BroadcastMessage(string message, string ip)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+
+            for (int i = 0; i < clients.Count; i++)
+                if (clients[i].Address.ToString() != ip)
+                    listeningSocket.SendTo(data, clients[i]);
+        }
+
+
+        private static void Close()
+        {
+            if (listeningSocket != null)
+            {
+                listeningSocket.Shutdown(SocketShutdown.Both);
+                listeningSocket.Close();
+                listeningSocket = null;
+            }
+
+            Console.WriteLine("server stopped");
+        }
+    }
+}
